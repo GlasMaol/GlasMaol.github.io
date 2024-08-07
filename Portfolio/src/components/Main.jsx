aec67de10fc66f1a1cffe61d70842d77eee07931
Alistair McCann <allyjmccann@gmail.com>
2024-08-07 12:15:31 +0200

    added logos, started on main sections

---
+++ Portfolio/src/components/Main.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <main>
+            <section className='frontendSection'>
+                <article>
+                    <h1 className='h1ContentTitle'>FRONTEND</h1>
+                    <h3 className='h3ContentSubTitle'>Resan som frontendare</h3>
+                    <p>Här kommer mer text snart</p>
+                </article>
+                <article>
+                    <img src="./src/assets/html-logo.png" alt="" />
+                    <img src="./src/assets/CSS-Logo.png" alt="" />
+                    <img src="./src/assets/Javascript-logo.png" alt="" />
+                    <img src="./src/assets/figma-logo.png" alt="" />
+                    <img src="./src/assets/React-icon.png" alt="" />
+                    <img src="./src/assets/Node.js_logo.png" alt="" />
+                </article>
+            </section>
+
+            <section className='projektOmSection'>
+                <article>
+                    <h1>PROJEKT</h1>
+                    <img src="./src/assets/alpineSportlogo-whitetext.svg" alt="Alpine Sport logo" />
+                    <section>
+                        <article>
+                            <p>Här kommer mer text snart</p>
+                        </article>
+                        <article>
+                            <article>
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                            </article>
+                        </article>
+                    </section>
+                </article>
+                <article>
+                    <h1>OM</h1>
+                    <h3>Vem är jag</h3>
+                    <section>
+                        <article>
+
+                        </article>
+                        <article>
+
+                        </article>
+                    </section>
+                </article>
+            </section>
+        </main>
+    )
+}
+
+export default Main
