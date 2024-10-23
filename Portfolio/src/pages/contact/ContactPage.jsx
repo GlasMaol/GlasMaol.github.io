7d627394e42104fdbee783d9be85464495fbbad8
Alistair McCann <allyjmccann@gmail.com>
2024-10-23 21:53:33 +0200

    added contact form and connected to emailJS, lösen kopplad till en .env och gitignored

--- Portfolio/src/pages/contact/ContactPage.jsx
+++ Portfolio/src/pages/contact/ContactPage.jsx
@@ -14,13 +14,14 @@ function ContactPage() {
 
     emailjs.sendForm(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        form.current,
-        import.meta.env.VITE_EMAILJS_USER_ID
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      form.current,
+      import.meta.env.VITE_EMAILJS_USER_ID
     )
       .then((result) => {
         console.log(result.text);
         setMessageSent(true);
+        form.current.reset();
       }, (error) => {
         console.log(error.text);
         setError('An error occurred while sending the message. Please try again later.');
@@ -31,20 +32,12 @@ function ContactPage() {
     <div>
       <ContentPageHeader />
       <section className='contact-container glassEffectDark'>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>
-            Name:
-            <input type="text" name="user_name" required />
-          </label>
-          <label>
-            Email:
-            <input type="email" name="user_email" required />
-          </label>
-          <label>
-            Message:
-            <textarea name="message" required />
-          </label>
-          <button type="submit">Send</button>
+        <h1 className='h1-contact'>CONTACT</h1>
+        <form className='form' ref={form} onSubmit={sendEmail}>
+          <input className='name-input' type="text" name="user_name" placeholder="Namn" required />
+          <input className='email-input' type="email" name="user_email" placeholder="E-post" required />
+          <textarea className='text-input' name="message" placeholder="Meddelande" required />
+          <button className='form-btn' type="submit">Skicka</button>
         </form>
         {messageSent && <p>Message sent successfully!</p>}
         {error && <p>{error}</p>}
@@ -55,4 +48,6 @@ function ContactPage() {
 }
 
 export default ContactPage;
+
+
 
